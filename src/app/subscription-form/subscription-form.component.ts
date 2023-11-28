4fafd72108e029f2d953907ab1a5211137169968
Chanuth <it20074418@my.sliit.lk>
2023-11-28 19:21:31 +0530

    implemen subscription form

--- src/app/subscription-form/subscription-form.component.ts
+++ src/app/subscription-form/subscription-form.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-subscription-form',
   templateUrl: './subscription-form.component.html',
-  styleUrl: './subscription-form.component.css'
+  styleUrls: ['./subscription-form.component.css']
 })
 export class SubscriptionFormComponent {
 
