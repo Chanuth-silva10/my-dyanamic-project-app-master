533cf0d29e02cca683ee9c5f09bfbd5f6aee3b49
Chanuth <it20074418@my.sliit.lk>
2023-11-29 09:41:06 +0530

    implement category selection logic

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -10,7 +10,9 @@ export class PostsService {
 
   loadFeatured() {
     return this.afs
-      .collection('posts', ref => ref.where('isFeatured', '==', true).limit(4))
+      .collection('posts', (ref) =>
+        ref.where('isFeatured', '==', true).limit(4)
+      )
       .snapshotChanges()
       .pipe(
         map((actions) => {
@@ -25,7 +27,24 @@ export class PostsService {
 
   loadLatest() {
     return this.afs
-      .collection('posts', ref => ref.orderBy('createdAt'))
+      .collection('posts', (ref) => ref.orderBy('createdAt'))
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, data };
+          });
+        })
+      );
+  }
+
+  loadCategoryPosts(categoryId: any) {
+    return this.afs
+      .collection('posts', (ref) =>
+        ref.where('category.categoryId', '==', categoryId).limit(4)
+      )
       .snapshotChanges()
       .pipe(
         map((actions) => {
