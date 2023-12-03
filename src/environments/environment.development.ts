01418215342d8baa6ae05cbf0b9429fe5fa60691
Chanuth <it20074418@my.sliit.lk>
2023-12-03 15:25:36 +0530

    deploy firebase using github action

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,11 @@
 export const environment = {
-    production: true,
-    firebaseConfig: {
-      apiKey: 'AIzaSyAgA8kZqBPL8TEApV7H9PA_vH_zHP0DAdk',
-      authDomain: 'ang-blog-f542d.firebaseapp.com',
-      projectId: 'ang-blog-f542d',
-      storageBucket: 'ang-blog-f542d.appspot.com',
-      messagingSenderId: '905020878928',
-      appId: '1:905020878928:web:f93d540196a7ec79802155',
-    },
-  };
-  
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBW3ZZ60p8hB8ouu3an5sq731BMxtbPYnM',
+    authDomain: 'project-blogger-345bb.firebaseapp.com',
+    projectId: 'project-blogger-345bb',
+    storageBucket: 'project-blogger-345bb.appspot.com',
+    messagingSenderId: '292139492321',
+    appId: '1:292139492321:web:da23d416956db2702928a0',
+  },
+};
