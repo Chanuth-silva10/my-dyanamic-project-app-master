deaab9ce70210425089b490e0da3b3d5fe3866bc
Chanuth <it20074418@my.sliit.lk>
2023-12-12 21:08:19 +0530

    add changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyBzKGn5h8pzd14saBF3zs1cFR2zRYb7bHg',
-    authDomain: 'project-blogger-ee575.firebaseapp.com',
-    projectId: 'project-blogger-ee575',
-    storageBucket: 'project-blogger-ee575.appspot.com',
-    messagingSenderId: '514333099648',
-    appId: '1:514333099648:web:892e2d284a0a7fcf744865',
+    apiKey: 'AIzaSyDzfcemqNIemmX82UgA9nNF3qNHDDUT0Ug',
+    authDomain: 'project-blogger-738ad.firebaseapp.com',
+    projectId: 'project-blogger-738ad',
+    storageBucket: 'project-blogger-738ad.appspot.com',
+    messagingSenderId: '875119155595',
+    appId: '1:875119155595:web:753d6534e43bd7b23f378e',
   },
 };
